c3f6bff78364ce10356f80afe9deef3a41f540e2
Sergei Smirnov <sergei.smirnov@bp.com>
2023-05-05 19:08:42 +0600

    App.js fix

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'app';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import TimeAgo from 'javascript-time-ago'
